39ce43c4ebf66d605b976954f01ff92b1cabf139
Michael Casey <caseymichaelt1893@gmail.com>
2021-01-14 14:51:37 -0500

    Added folder

---
+++ my-app/src/validation/validate.js
@@ -0,0 +1,13 @@
+function versionSearch(value){
+    let spec_chars = ["`", "~", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "+", "=", "{", "}", "[", "]", "\\", "|", 
+                    ":", ";", "'", '""', "'", "<", ",", ">", "?", "/"];
+    for(let i=0; i < value.length; i++){
+        for(let j=0; j < spec_chars.length; j++){
+            if(value[i] === spec_chars[j]){
+                return "Invalid inout no special characters allowed"
+            }   
+    
+        }
+    }
+    return '';
+}
